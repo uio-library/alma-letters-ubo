uio-library/alma-letters-ubo

commit 89cf8f63724250756e7899b1222b7256691e671f

FulOverdueAndLostLoanLetter: Use 'title_abcnph' for more details

xsl/letters/FulOverdueAndLostLoanLetter.xsl
@@ -11,7 +11,7 @@
 <xsl:include href="style.xsl"/>
 <xsl:include href="recordTitle.xsl"/>
 
-<!-- Templates -->
+<!-- ============================ Templates ============================ -->
 
 <!--
   Template to standardize table styling without too much inline code.
@@ -52,7 +52,7 @@
 
       Depends on: multilingual
     -->
-    <xsl:value-of select="title"/>
+    <xsl:value-of select="title_abcnph"/>
     <xsl:if test="material_type = 'ISSUE'">
         <xsl:call-template name="multilingual"><!-- header.xsl -->
             <xsl:with-param name="nb" select="', hefte'"/>
@@ -95,13 +95,13 @@
 </xsl:template>
 
 <!--
-  Template: formatLoan
+  Template: formatLoanWithFees
 
   Generates rows for the fines/fees associated with a single document/loan.
   Usually there will be either one (LOSTITEMREPLACEMENTFEE) or two rows
   (LOSTITEMREPLACEMENTFEE and LOSTITEMPROCESSFEE).
 -->
-<xsl:template name="formatLoan">
+<xsl:template name="formatLoanWithFees">
   <xsl:variable name="loan" select="."/>
   <xsl:variable name="fees" select="count(fines_fees_list/user_fines_fees)"/>
   <xsl:for-each select="fines_fees_list/user_fines_fees">
@@ -152,6 +152,7 @@
   </xsl:for-each>
 </xsl:template>
 
+<!-- ============================ The main part of the letter ============================ -->
 
 <!-- The letter itself -->
 
@@ -206,7 +207,7 @@
     </xsl:with-param>
     <xsl:with-param name="body">
       <xsl:for-each select="display_list/overdue_and_lost_loan_notification_display">
-        <xsl:call-template name="formatLoan"></xsl:call-template>
+        <xsl:call-template name="formatLoanWithFees"></xsl:call-template>
       </xsl:for-each>
     </xsl:with-param>
   </xsl:call-template>

commit 411c3621de1c3b44142bc3e1b35a5353d1a5ec8c

Fix bug introduced in 89cf8f63

xsl/letters/FulOverdueAndLostLoanLetter.xsl
@@ -52,17 +52,17 @@
 
       Depends on: multilingual
     -->
-    <xsl:value-of select="title_abcnph"/>
-    <xsl:if test="material_type = 'ISSUE'">
+    <xsl:value-of select="physical_item_display_for_printing/title_abcnph"/>
+    <xsl:if test="item_loan/material_type = 'ISSUE'">
         <xsl:call-template name="multilingual"><!-- header.xsl -->
             <xsl:with-param name="nb" select="', hefte'"/>
             <xsl:with-param name="nn" select="', hefte'"/>
             <xsl:with-param name="en" select="', issue'"/>
         </xsl:call-template>
     </xsl:if>
-    <xsl:if test="item_description != ''">
+    <xsl:if test="item_loan/item_description != ''">
         <xsl:text> </xsl:text>
-        <xsl:value-of select="item_description"/><!-- Such as "53(2008) 11" -->
+        <xsl:value-of select="item_loan/item_description"/><!-- Such as "53(2008) 11" -->
     </xsl:if>
 </xsl:template>
 
@@ -119,7 +119,7 @@
           <xsl:attribute name="rowspan">
             <xsl:value-of select="$fees"/>
           </xsl:attribute>
-          <xsl:for-each select="$loan/item_loan">
+          <xsl:for-each select="$loan">
             <!-- Not really a loop, we use for-each just to change scope to item_loan -->
             <xsl:call-template name="formatItemTitle"></xsl:call-template>
           </xsl:for-each>

xsl/letters/FulOverdueAndLostLoanNotificationLetter.xsl
@@ -55,7 +55,7 @@
     </xsl:when>
     <xsl:otherwise>
       <xsl:call-template name="multilingual"><!-- header.xsl -->
-        <xsl:with-param name="nb" select="'Må leveres."/><!-- Fornyes automatisk? -->
+        <xsl:with-param name="nb" select="'Må leveres.'"/><!-- Fornyes automatisk? -->
         <xsl:with-param name="nn" select="'Må leverast.'"/>
         <xsl:with-param name="en" select="'Must be returned.'"/><!-- Renewed automatically -->
       </xsl:call-template>